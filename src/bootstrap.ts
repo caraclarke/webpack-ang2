5ce26cc727e3ad34255325687ce158d82f4f0f66
caraclarke <cara.clarke8@gmail.com>
2016-08-09 12:30:34 -0400

    i broke it

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,15 +1,13 @@
-import {enableProdMode, provide} from "angular2/core";
-import {bootstrap, ELEMENT_PROBE_PROVIDERS} from 'angular2/platform/browser';
-import {ROUTER_PROVIDERS} from 'angular2/router';
-import {HashLocationStrategy, LocationStrategy} from 'angular2/platform/common';
-import {HTTP_PROVIDERS} from 'angular2/http';
+import {enableProdMode, provide} from "@angular/core";
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
+import {HTTP_PROVIDERS} from '@angular/http';
 
 const ENV_PROVIDERS = [];
 // depending on the env mode, enable prod mode or add debugging modules
 if (process.env.ENV === 'build') {
   enableProdMode();
-} else {
-  ENV_PROVIDERS.push(ELEMENT_PROBE_PROVIDERS);
 }
 
 /*
